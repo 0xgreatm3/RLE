6e241ea5f128b5fc3c06ad58a413f184963ad7b8
Great <ezennagreat@gmail.com>
2025-02-12 09:38:29 +0100

    Building out the logic, testing

--- rle.js
+++ rle.js
@@ -1,5 +1,17 @@
 // The compress() function
-function compress(string) {}
+function compress(string) {
+  let result = "";
+  if (!typeof string === "string") {
+    throw new Error("Input must be a string");
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1]) {
+      debugger;
+      result += string[i];
+    }
+  }
+}
 
 // The decompress() function
 function decompress(string) {}
