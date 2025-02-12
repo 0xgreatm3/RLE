093506259e43ac3f274a61865a262304b0afa8f3
Great <ezennagreat@gmail.com>
2025-02-12 09:58:28 +0100

    added a bridge to check if character is letter

--- rle.js
+++ rle.js
@@ -6,12 +6,17 @@ function compress(string) {
   }
 
   for (let index = 0; index < string.length; index++) {
-    const char = string[index]
-    if(resultObj[char]) {
-        resultObj[char]++
-    }
+    const char = string[index];
+    if (
+      (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || // Uppercase A-Z
+      (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) // Lowercase a-z
+    ) {
+      if (resultObj[char]) {
+        resultObj[char]++;
+      }
 
-    resultObj[char]++
+      resultObj[char]++;
+    }
   }
 
   return resultObj;
@@ -20,5 +25,5 @@ function compress(string) {
 // The decompress() function
 function decompress(string) {}
 
-let testString = compress("AAAAABBB")
-console.log(testString)
+let testString = compress("AAAAABBB!CCCC3333");
+console.log(testString);
