cee1d51ec022864b48ba195621dafb2739e480cf
Great <ezennagreat@gmail.com>
2025-02-13 17:39:33 +0100

    Remove the consecutive function, after i've added it to the compress function

--- rle.js
+++ rle.js
@@ -1,35 +1,9 @@
 // The compress() function
-function compress(string) {
-  let resultObj = {};
-  let resul = "";
+function compress(string) { 
   if (!(typeof string === "string")) {
     throw new Error("Input must be a string");
   }
 
-  for (let index = 0; index < string.length; index++) {
-    const char = string[index];
-    if (
-      (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || // Uppercase A-Z
-      (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) // Lowercase a-z
-    ) {
-      if (!resultObj[char]) {
-        resultObj[char] = 0;
-      }
-
-      resultObj[char]++;
-    }
-  }
-
-  return resultObj;
-}
-
-// The decompress() function
-function decompress(string) {}
-
-// let testString = compress("AAAABBBBCCCCEEFFGHS");
-// console.log(testString);
-
-function consecutive(string) {
   let result = "";
   let count = 1;
   for (let index = 1; index < string.length; index++) {
@@ -46,4 +20,8 @@ function consecutive(string) {
   return result;
 }
 
-console.log(consecutive("AAABBBBBAA!AAAAA"));
+// The decompress() function
+function decompress(string) {}
+
+let testString = compress("AAAABBBBCCCCEEFFGHS");
+console.log(testString);  
