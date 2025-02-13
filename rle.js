3da7ff850e14e8d9b974eb50d48623a1ea2e1903
Great <ezennagreat@gmail.com>
2025-02-13 18:02:39 +0100

    Refactor compress function to streamline character checks and ensure proper return of result

--- rle.js
+++ rle.js
@@ -3,15 +3,16 @@ function compress(string) {
   if (!(typeof string === "string")) {
     throw new Error("Input must be a string");
   }
-  if (
-    (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || // Uppercase A-Z
-    (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) // Lowercase a-z
-  ) {
-    let result = "";
-    let count = 1;
-    for (let index = 1; index < string.length; index++) {
-      const char = string[index];
-      const prevChar = string[index - 1];
+
+  let result = "";
+  let count = 1;
+  for (let index = 1; index < string.length; index++) {
+    const char = string[index];
+    const prevChar = string[index - 1];
+    if (
+      (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || // Uppercase
+      (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) // Lowercase a-z
+    ) {
       if (char === prevChar) {
         count++;
       } else {
@@ -20,10 +21,11 @@ function compress(string) {
       }
     }
   }
+  return result;
 }
 
 // The decompress() function
 function decompress(string) {}
 
-// let testString = compress("AAAABBBBCCCCEEFFGHS");
-// console.log(testString);
+let testString = compress("AAAbbb909099");
+console.log(testString);
