f2e2ece346bc5229f27c36eb23ebc07cc49cabe5
Great <ezennagreat@gmail.com>
2025-02-13 17:42:21 +0100

    Added check for Alpha Characters

--- rle.js
+++ rle.js
@@ -1,27 +1,29 @@
 // The compress() function
-function compress(string) { 
+function compress(string) {
   if (!(typeof string === "string")) {
     throw new Error("Input must be a string");
   }
-
-  let result = "";
-  let count = 1;
-  for (let index = 1; index < string.length; index++) {
-    const char = string[index];
-    const prevChar = string[index - 1];
-    if (char === prevChar) {
-      count++;
-    } else {
-      result += count + prevChar;
-      count = 1;
+  if (
+    (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || // Uppercase A-Z
+    (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) // Lowercase a-z
+  ) {
+    let result = "";
+    let count = 1;
+    for (let index = 1; index < string.length; index++) {
+      const char = string[index];
+      const prevChar = string[index - 1];
+      if (char === prevChar) {
+        count++;
+      } else {
+        result += count + prevChar;
+        count = 1;
+      }
     }
   }
-  result += count + string[string.length - 1];
-  return result;
 }
 
 // The decompress() function
 function decompress(string) {}
 
-let testString = compress("AAAABBBBCCCCEEFFGHS");
-console.log(testString);  
+// let testString = compress("AAAABBBBCCCCEEFFGHS");
+// console.log(testString);
