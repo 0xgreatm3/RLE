22450335ea8687892dccb27127aab2683966264e
Great <ezennagreat@gmail.com>
2025-02-21 10:56:05 +0100

    Simplify compress function by removing unnecessary conditionals and improving readability

--- rle.js
+++ rle.js
@@ -3,28 +3,24 @@ function compress(stringParam) {
     throw new Error("Input must be a string");
   }
 
-  if (stringParam.length === 0) {
-    return "";
-  }
+  if (stringParam.length === 0) return "";
 
   const regex = /[A-Za-z]/g;
   const string = (stringParam.match(regex) || []).join("");
 
   // In case the string is empty after removing non-alphabetic characters
-  if (string.length === 0) {
-    return "";
-  }
+  if (string.length === 0) return "";
 
   // In case the string has only one character
-  if (string.length === 1) {
-    return string + "1";
-  }
+  if (string.length === 1) return string + "1";
 
   let result = "";
   let count = 1;
+
   for (let index = 1; index < string.length; index++) {
     const char = string[index];
     const prevChar = string[index - 1];
+
     if (char === prevChar) {
       count++;
     } else {
@@ -32,7 +28,10 @@ function compress(stringParam) {
       count = 1;
     }
   }
+
+  // Add the last character and its count
   result += string[string.length - 1] + count;
+
   return result;
 }
 
