6984ae75d108ff18d147e993a4875c94f6a4978a
Great <ezennagreat@gmail.com>
2025-03-12 10:45:17 +0100

    Refactor compress function to handle non-alphabetic characters and adjust output format

--- rle.js
+++ rle.js
@@ -3,28 +3,19 @@ function compress(stringParam) {
     throw new Error("Input must be a string");
   }
 
-  if (stringParam.length === 0) return "";
-
-  const regex = /[A-Za-z]/g;
-  const string = (stringParam.match(regex) || []).join("");
-
-  // In case the string is empty after removing non-alphabetic characters
-  if (string.length === 0) return "";
-
-  // In case the string has only one character
-  if (string.length === 1) return string + "1";
+  if (stringParam.length === 0) return "";  
 
   let result = "";
   let count = 1;
 
-  for (let index = 1; index < string.length; index++) {
+  for (let index = 1; index < stringParam.length; index++) {
     const char = string[index];
     const prevChar = string[index - 1];
 
     if (char === prevChar) {
       count++;
     } else {
-      result += prevChar + count;
+      result += /\d/.test(prevChar) ? `[${prevChar}]` : prevChar + count;
       count = 1;
     }
   }
